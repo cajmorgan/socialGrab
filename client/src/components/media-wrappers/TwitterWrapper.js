671ac8d9740b5bdde450881e322e3583dd3984dc
Caj <caj@cajmorgan>
2021-11-12 10:06:33 +0100

    ok

--- client/src/components/media-wrappers/TwitterWrapper.js
+++ client/src/components/media-wrappers/TwitterWrapper.js
@@ -19,7 +19,7 @@ function TwitterWrapper({ data }) {
           <img src={data.profilePic} />
         </div>
         <div className="header__bio-wrapper">
-          <h2 className="bio-wrapper__name">@{data.bio.name}</h2>
+          <h2 className="bio-wrapper__name">{data.bio.name} <span>{data.tweets[0].username}</span></h2>
           <div className="bio-wrapper__user-info">
             <h3>{data.bio.tweetsNumber}</h3>
             <h3>{data.bio.followers}</h3>
